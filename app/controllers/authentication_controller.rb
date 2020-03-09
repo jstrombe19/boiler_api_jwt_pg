5bdd11103fe90c997752d78b01c66abde3b8de3d
jstrombe19 <jaredstromberg@gmail.com>
2020-03-09 12:34:11 -0600

    Final commit after verifying all actions are functinoal. This repo will be shared on the Medium article documenting its creation for reference.

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,14 +1,14 @@
 class AuthenticationController < ApplicationController
 
   def login
-    user = User.find_by(username: params[:username])
+    @user = User.find_by(username: params[:username])
 
-    if !user 
+    if !@user 
       render status: 401
     else 
-      if user.authenticate(params[:password])
+      if @user.authenticate(params[:password])
         secret_key = Rails.application.secrets.secret_key_base[0]
-        token = JWT.encode({user_id: user.id}, secret_key)
+        token = JWT.encode({user_id: @user.id}, secret_key)
 
         render json: {token: token}
       else
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
 
   def create
-    user = User.new(user_params)
-    if user.save
-      render json: user 
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user 
     else
       render status: 418
     end
