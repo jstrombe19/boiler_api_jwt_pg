f76f9aa1982eff21edc8b675b85f580b559ce22b
jstrombe19 <jaredstromberg@gmail.com>
2020-03-08 22:20:32 -0600

    Built out users controller and authentication controller. Verified create and login routes, but cannot successfully hit profile or update. Route verification performed using Postman. Current error for profile is wrong number of arguments given: 0, expected: 1.

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,4 +1,20 @@
-require 'jwt'
+class AuthenticationController < ApplicationController
 
-class AuthenticationController
+  def login
+    user = User.find_by(username: params[:username])
+
+    if !user 
+      render status: 401
+    else 
+      if user.authenticate(params[:password])
+        secret_key = Rails.application.secrets.secret_key_base[0]
+        token = JWT.encode({user_id: user.id}, secret_key)
+
+        render json: {token: token}
+      else
+        render status: 418
+      end
+    end
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :is_user_verified?, only: [:profile]
+  before_action :is_user_authorized?, only: [:profile, :update, :destroy]
 
   def create
     user = User.new(user_params)
@@ -11,6 +11,16 @@ class UsersController < ApplicationController
   end
   
   def profile
+    render json: @user 
+  end
+  
+  def update
+    @user.update(user_params)
+    render json: @user 
+  end
+
+  def destroy 
+    @user.destroy 
   end
 
   private
@@ -19,13 +29,17 @@ class UsersController < ApplicationController
     params.require('user').permit([:username, :password])
   end
 
-  def is_user_verified? (request)
-    user = User.find(params[:id])
-
-    if !user 
+  def is_user_authorized?(request)
+    authorization_header = request.headers[:authorization]
+    if !authorization_header 
       render status: 401
     else
-      
+      token = authorization_header.split(" ")[1]
+      secret_key = Rails.application.secrets.secret_key_base[0]
+      decoded_token = JWT.decode(token, secret_key)
+      @user = User.find(decoded_token[0]["user_id"])
+    end
+    @user
   end
 
 end
